88a54a653530596cbafa3103dd6f74eae624ed64
dave-woods <dwoods@tcd.ie>
2015-03-07 19:53:46 +0100

    Added GLFW to project created basic windowing
    Create window and toggle between fullscreen and windowed mode.

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ A game developed in C++ in order to increase knowledge of the language and pract
 Following an online tutorial, found [here](https://www.youtube.com/watch?v=fRUYl6_5m3o&index=1&list=PL93bFkoCMJslJJb15oQddnmABNUl6iz8e).
 
 This is NOT original code, and I do not claim ownership of the idea, nor the code itself.
+
+
+Update/Progress To Date
+-----------------------
+
+* Setup Visual Studio for use with project
+* Added GLFW to project, and created basic windowing functionality
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,82 @@
 #include <Dunjun/Common.hpp>
 
+#include<GLFW/glfw3.h>
+
 #include <iostream>
+
+GLOBAL const int g_windowWidth = 854;
+GLOBAL const int g_windowHeight = 480;
+GLOBAL const char* windowTitle = "Dunjun v0.0.1";
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hi there" << std::endl;
-	std::cin.ignore();
-	return 0;
+	GLFWwindow* window;
+
+	/*Initialise the library*/
+	if (!glfwInit())
+		return EXIT_FAILURE;
+
+	/*Create a windowed mode window and its OpenGL context*/
+	window = glfwCreateWindow(g_windowWidth, g_windowHeight, windowTitle, nullptr, nullptr);
+	if (!window)
+	{
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	/*Make the window's context current*/
+	glfwMakeContextCurrent(window);
+
+	bool fullscreen = false;
+	bool running = true;
+	/*Loop until the user closes the window*/
+	while (running)
+	{
+		/*Default pixel value*/
+		glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+
+
+		/*Render here*/
+
+		/*Swap front and back buffers*/
+		glfwSwapBuffers(window);
+
+		/*Poll for and process events*/
+		glfwPollEvents();
+
+
+		if (glfwWindowShouldClose(window) || glfwGetKey(window, GLFW_KEY_ESCAPE))
+			running = false;
+
+		if (glfwGetKey(window, GLFW_KEY_F11))
+		{
+			fullscreen = !fullscreen;
+			
+			GLFWwindow* newWindow;
+			if (fullscreen)
+			{
+				/*Get number of modes*/
+				int count;
+				/*Getting the monitors width and height*/
+				const GLFWvidmode* modes = glfwGetVideoModes(glfwGetPrimaryMonitor(), &count);
+
+				newWindow = glfwCreateWindow(modes[count - 1].width, modes[count - 1].height, windowTitle, glfwGetPrimaryMonitor(), window);
+			}
+			else
+			{
+				newWindow = glfwCreateWindow(g_windowWidth, g_windowHeight, windowTitle, nullptr, window);
+			}
+			glfwDestroyWindow(window);
+			window = newWindow;
+			glfwMakeContextCurrent(window);
+		}
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return EXIT_SUCCESS;
 }
